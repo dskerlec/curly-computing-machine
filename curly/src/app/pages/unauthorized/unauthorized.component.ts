e6012d3d4e57ab15916cf3384ad63c7cf1cab6fb
Dalton Skerlec <dskerlec@gmail.com>
2024-04-20 10:25:58 -0400

    init commit, set up basic routing

---
+++ curly/src/app/pages/unauthorized/unauthorized.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorized',
+  templateUrl: './unauthorized.component.html',
+  styleUrls: ['./unauthorized.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
+})
+export class UnauthorizedComponent {
+
+}
